d38e3ca9cb5504cfaa681c988d78a22633ebc3bb
Leas <leo@porscha.eu>
2020-10-26 21:00:07 +0100

    Started Shutdown

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 import secrets
-bot = discord.Client()
+import asyncio
 
-bot = commands.Bot(command_prefix='$')
+client = commands.Bot(command_prefix='$')
 
-@bot.event
+@client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
-    
-@bot.command()
+    print('We have logged in as {0.user}'.format(client))
+
+@client.command()
 async def ping(ctx):
-    await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))
+    await ctx.send('Pong! {0} seconds'.format(round(client.latency, 3)))
 
+@bot.command()
+async def shutdown(ctx, args: int):
+    member = ctx.author
+    if args != null:
+        if ctx.message.author.server_permission.administrator:
+            try:
+                asyncio.wait(args)
+            except:
+                await ctx.send("You may only use an Integer as an argument!")
 
+def checkperms(member, permission):
 
-bot.run(secrets.token)
+client.run(secrets.token)
