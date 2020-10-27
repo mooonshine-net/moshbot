3a16efddbb429bf5cec8d8e2ef66f10193fa388c
Leas <leo@porscha.eu>
2020-10-27 14:27:00 +0100

    Shutdown Command done

--- main.py
+++ main.py
@@ -13,16 +13,27 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('Pong! {0} seconds'.format(round(client.latency, 3)))
 
-@bot.command()
-async def shutdown(ctx, args: int):
-    member = ctx.author
-    if args != null:
-        if ctx.message.author.server_permission.administrator:
-            try:
-                asyncio.wait(args)
-            except:
-                await ctx.send("You may only use an Integer as an argument!")
+@client.command()
+@commands.has_permissions(administrator=True)
+async def shutdown(ctx, arg1: int):
+    await ctx.send("Logging out in {} seconds".format(arg1))
+    await asyncio.sleep(arg1)
+    await ctx.send("🌄 Goodbye")
+    await client.logout()
 
-def checkperms(member, permission):
+@shutdown.error
+async def shutdown_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Logging out in 10 seconds")
+        await asyncio.sleep(10)
+        await ctx.send("🌄 Goodbye")
+        await client.logout()
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("⚠ You may only use an Integer for an argument {}!".format(ctx.message.author.mention))
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("⚠ You dont have the required permissions to execute this command {}!".format(ctx.message.author.mention))
+    else:
+        await ctx.send("⚠ Something went wrong while executing this command {}!".format(ctx.message.author.mention))
+
 
 client.run(secrets.token)
